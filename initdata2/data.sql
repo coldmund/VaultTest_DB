fc279b7037b95af16d9f4bc3c435f2272a40668b
coldmund <coldmund@gmail.com>
2022-02-02 22:58:18 +0900

    add db volume

--- initdata2/data.sql
+++ initdata2/data.sql
@@ -1,10 +1,3 @@
 CREATE DATABASE IF NOT EXISTS vaultdb;
 
-USE vaultdb;
-CREATE TABLE `vault_kv_store` (
-  `key` varchar(100) NOT NULL,
-  `value` text NOT NULL,
-  PRIMARY KEY (`key`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
 GRANT ALL ON `vaultdb`.* TO 'user'@'%';
