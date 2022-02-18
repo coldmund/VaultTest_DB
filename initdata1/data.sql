e435a5d3f63800881aa73652ec4e747f18220270
Hong, Joon-yong <joonyong.hong@lge.com>
2022-02-18 16:36:57 +0900

    change table name(use lower case only)

--- initdata1/data.sql
+++ initdata1/data.sql
@@ -1,15 +1,15 @@
 CREATE DATABASE IF NOT EXISTS testdb;
 
 USE testdb;
-CREATE TABLE `TestTable` (
+CREATE TABLE `test_table` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `cle` varchar(100) NOT NULL,
   `prix` text NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
 
-LOCK TABLES `TestTable` WRITE;
-INSERT INTO `TestTable` VALUES (1,'key1','value1');
+LOCK TABLES `test_table` WRITE;
+INSERT INTO `test_table` VALUES (1,'key1','value1');
 UNLOCK TABLES;
 
 GRANT ALL ON `testdb`.* TO 'user'@'%';
